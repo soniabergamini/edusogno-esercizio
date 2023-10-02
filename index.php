69c567b3ebf21444620f6a05a9f1a9c9abb0bd3c
soniabergamini <sounia96@outlook.it>
2023-10-02 19:15:13 +0200

    style: add helpers, root, general

--- index.php
+++ index.php
@@ -5,11 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="./assets/img/favicon.ico">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;9..40,700&display=swap" rel="stylesheet">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="./assets/styles/helpers.css">
+    <link rel="stylesheet" href="./assets/styles/style.css">
+    <!-- Title -->
     <title>Edusogno</title>
 </head>
 
 <body>
-
+    <header class="radiusHeader bgWhite dFlex flexAlignCtr px2rem">
+        <img src="./assets/img/logo.png" alt="edusogno-logo">
+    </header>
+    <main>
+        <p>this is a paragraph</p>
+    </main>
 </body>
 
 </html>
