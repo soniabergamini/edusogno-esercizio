dca31d76072558cc2a71a6b6cea531ed819d199d
soniabergamini <sounia96@outlook.it>
2023-10-06 12:02:49 +0200

    feat(login): add reset password button

--- partials/main-login.php
+++ partials/main-login.php
@@ -8,14 +8,24 @@
             <p class="txtAlignCtr">🚫 <?= $session['login_error'] ?></p>
         </div>
 
-        <!-- Form Inputs & Submit -->
+        <!-- Form Inputs -->
         <label class="dBlock txtSmall txtDark" for="email"><strong>Inserisci l'email</strong></label>
         <input class="dBlock mInput w100 borderInput" type="email" name="email" id="email" placeholder="name@example.com" value="<?= $session['login_email'] ?? '' ?>" required>
+
         <label class="dBlock txtSmall txtDark" for="password"><strong>Inserisci la password</strong></label>
         <div class="dFlex flexAlignCtr w100 mInput borderInput">
             <input type="password" name="password" id="password" placeholder="Scrivila qui" required class="border0 flex1 pr2rem">
             <img id="passSecurity" src="../assets/img/EyeVector.png" alt="view-pass-icon" class="pointer">
         </div>
+
+        <!-- Reset Password -->
+        <p class="txtAlignR txtSmall">
+            <a href="#" id="resetPass" class="txtDecNone txtRed txtSmall txtUnderHov">
+                Password dimenticata? <strong>Clicca QUI</strong>
+            </a>
+        </p>
+        
+        <!-- Submit Button -->
         <button type="submit" class="radiusBtn w100 bgBlue txtWhite py08rem border0 my2rem pointer"><strong>ACCEDI</strong></button>
 
         <!-- Redirect to Register Route -->
