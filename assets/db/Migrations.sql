0ecbf57950c32fbe8920d3b24367b8522d037b16
soniabergamini <sounia96@outlook.it>
2023-10-07 15:45:16 +0200

    feat(migrations): add event description

--- assets/db/Migrations.sql
+++ assets/db/Migrations.sql
@@ -14,9 +14,10 @@ id int NOT NULL AUTO_INCREMENT,
 attendees text,
 nome_evento varchar(255),
 data_evento datetime,
+descrizione text,
 PRIMARY KEY (id)
 );
 
-INSERT INTO `eventi`(`attendees`, `nome_evento`, `data_evento`) VALUES ('ulysses200915@varen8.com,qmonkey14@falixiao.com,mavbafpcmq@hitbase.net','Test Edusogno 1', '2022-10-13 14:00'), ('dgipolga@edume.me,qmonkey14@falixiao.com,mavbafpcmq@hitbase.net','Test Edusogno 2', '2022-10-15 19:00'), ('dgipolga@edume.me,ulysses200915@varen8.com,mavbafpcmq@hitbase.net','Test Edusogno 3', '2022-10-15 19:00');
+INSERT INTO `eventi`(`attendees`, `nome_evento`, `data_evento`, `descrizione`) VALUES ('ulysses200915@varen8.com,qmonkey14@falixiao.com,mavbafpcmq@hitbase.net','Test Edusogno 1', '2022-10-13 14:00', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'), ('dgipolga@edume.me,qmonkey14@falixiao.com,mavbafpcmq@hitbase.net','Test Edusogno 2', '2022-10-15 19:00', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'), ('dgipolga@edume.me,ulysses200915@varen8.com,mavbafpcmq@hitbase.net','Test Edusogno 3', '2022-10-15 19:00', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
 
 INSERT INTO `utenti`(`nome`, `cognome`, `email`, `password`, `ruolo`) VALUES ('Marco','Rossi', 'ulysses200915@varen8.com', 'Edusogno123', 'utente'), ('Filippo', 'D\'Amelio', 'qmonkey14@falixiao.com','Edusogno?123', 'utente'), ('Gian Luca', 'Carta', 'mavbafpcmq@hitbase.net', 'EdusognoCiao', 'utente'), ('Stella', 'De Grandis', 'dgipolga@edume.me', 'EdusognoGia', 'admin');
